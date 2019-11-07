24222cfc1802607f815f1839a4600debb587dda9
Deanna PIEDRA <dpiedra@student.42.fr>
2019-11-07 20:06:19 +0100

    COMPLETED PART 1 FUNCTIONS :)

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 18:15:17 by dpiedra           #+#    #+#             */
-/*   Updated: 2019/11/07 19:40:18 by dpiedra          ###   ########.fr       */
+/*   Updated: 2019/11/07 20:02:28 by dpiedra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t i;
-	size_t len;
 
 	i = 0;
-	len = ft_strlen(dst);
-	while (src[i] != '\0' && len + i + 1 < dstsize)
+	if (!src || !dst)
+		return (0);
+	if (!dstsize)
+		return (ft_strlen(src));
+	while (src[i] != '\0' && i + 1 < dstsize)
 	{
-		dst[len + i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
-	dst[len + i] = '\0';
+	dst[i] = '\0';
 	i = ft_strlen(src);
 	return (i);
 }
