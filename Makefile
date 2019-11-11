5d461cde15e90f5c05836ee3f60c17f6e62d3c2f
Deanna PIEDRA <dpiedra@student.42.fr>
2019-11-11 16:12:13 +0100

    strtrim inlcuded

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/11/07 14:03:22 by dpiedra           #+#    #+#              #
-#    Updated: 2019/11/08 17:23:10 by dpiedra          ###   ########.fr        #
+#    Updated: 2019/11/11 15:40:07 by dpiedra          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -26,7 +26,7 @@ SRCS	=	ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c \
 			ft_memmove.c ft_memset.c ft_strchr.c ft_strdup.c \
 			ft_strlcat.c ft_strlcpy.c ft_strlen.c ft_strncmp.c \
 			ft_strnstr.c ft_strrchr.c ft_tolower.c ft_toupper.c \
-			ft_substr.c ft_strjoin.c 
+			ft_subtsr.c ft_strjoin.c  ft_strtrim.c
 			
 OBJ		=	$(SRCS:.c=.o)
 
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,14 +6,48 @@
 /*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 17:24:54 by dpiedra           #+#    #+#             */
-/*   Updated: 2019/11/08 17:40:34 by dpiedra          ###   ########.fr       */
+/*   Updated: 2019/11/11 16:11:20 by dpiedra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char *ft_strtrim(char const *s1, char const *set)
+int		ft_issep(char c, const char *set)
 {
-	
+	int i;
+
+	i = 0;
+	while (set[i] != '\0')
+	{
+		if (c == set[i])
+			return (1);
+		else
+			i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*newstr;
+	size_t	newlen;
+	int		start;
+	int		end;
+
+	newlen = 0;
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_issep(s1[start], set) == 1)
+		start++;
+	while (ft_issep(s1[end], set) == 1)
+		end--;
+	newlen = end + 1 - start;
+	if (!(newstr = malloc(sizeof(char) * (newlen + 1))))
+		return (NULL);
+	newstr = ft_substr(s1, start, newlen);
+	newstr[newlen] = '\0';
+	return (newstr);
 }
