ffb789c7c05884bb24c0eca140dfdf165c1c6891
Deanna PIEDRA <dpiedra@student.42.fr>
2019-11-15 11:47:03 +0100

    good add back

--- ft_lastadd_back.c
+++ ft_lastadd_back.c
@@ -6,7 +6,7 @@
 /*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 14:41:08 by dpiedra           #+#    #+#             */
-/*   Updated: 2019/11/13 16:50:22 by dpiedra          ###   ########.fr       */
+/*   Updated: 2019/11/15 11:46:46 by dpiedra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 void	ft_lstadd_back(t_list **alst, t_list *new)
 {
+	t_list *tmp;
+
 	if (!alst || !new)
 		return ;
-	new->next = ft_lstlast(*alst);
-	*alst = new;
+	if (!*alst)
+	{
+		*alst = new;
+		return ;
+	}
+	tmp = ft_lstlast(*alst);
+	tmp->next = new;
 }
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,13 +6,16 @@
 /*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 15:52:58 by dpiedra           #+#    #+#             */
-/*   Updated: 2019/11/13 16:50:22 by dpiedra          ###   ########.fr       */
+/*   Updated: 2019/11/15 11:08:47 by dpiedra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-
+	if (!lst)
+		return ;
+	*lst = (*del)(*);
 }
