f35cb5bc8d312a1a373285c0029adb76bd9079a3
Deanna PIEDRA <dpiedra@student.42.fr>
2019-11-15 19:43:09 +0100

    protected substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 13:30:40 by dpiedra           #+#    #+#             */
-/*   Updated: 2019/11/11 16:11:22 by dpiedra          ###   ########.fr       */
+/*   Updated: 2019/11/15 19:42:27 by dpiedra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*substr;
 	size_t	n;
 
-	if (start >= ft_strlen(s))
-		len = 0;
 	if (!s)
 		return (NULL);
+	if (start >= ft_strlen(s))
+		len = 0;
 	if (!(substr = malloc(sizeof(char) * (len + 1))))
 		return (NULL);
 	n = 0;
